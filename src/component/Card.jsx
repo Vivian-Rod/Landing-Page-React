47a56959ce5e571333cd748c712a5e5276440422
Viviana Alday <146024145+Vivianaalday2023@users.noreply.github.com>
2024-08-02 19:20:13 -0400

    Add files via upload

---
+++ src/component/Card.jsx
@@ -0,0 +1,14 @@
+export function Card(){
+    return (
+    <div className="d-flex flex-wrap justify-content-around">
+    <div className="card" style={{width:"18rem"}}>
+    <img src="https://www.petdarling.com/wp-content/uploads/2018/12/munchkin.jpg" class="card-img-top" alt="..."/>
+    <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, praesentium eum consequuntur sunt atque amet distinctio earum sapiente at voluptatibus? Repellat facilis nulla beatae maxime natus minima. Illum, doloremque sunt?.</p>
+        <a href="#" class="btn btn-primary">Find Out More</a>
+    </div>
+    </div>
+    </div>
+    )
+};
